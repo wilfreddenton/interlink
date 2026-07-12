88faa7cb31f2e9212147510f95c26b40db64aabf
wilfreddenton <wilfred@hey.com>
2026-07-12 10:45:58 -0400

    Drop Intel-mac (x86_64-apple-darwin) target from release + npm wrapper

--- npm/install.js
+++ npm/install.js
@@ -15,7 +15,6 @@ const REPO = "wilfreddenton/praetor";
 const TARGETS = {
   "linux x64": "x86_64-unknown-linux-musl",
   "linux arm64": "aarch64-unknown-linux-musl",
-  "darwin x64": "x86_64-apple-darwin",
   "darwin arm64": "aarch64-apple-darwin",
   "win32 x64": "x86_64-pc-windows-msvc",
 };
